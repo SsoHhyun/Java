52e4172d0bdd861eaaeebb81c8ff0d151d16d412
SsoHhyun <janeqcitizen@naver.com>
2022-12-13 20:10:29 +0900

    December 13rd, java study ch2-3

---
+++ ch02/src/Ex2_12.java
@@ -0,0 +1,12 @@
+
+public class Ex2_12 {
+	public static void main(String[] args) {
+		String s = "3";
+		System.out.println(s.charAt(0) - '0');
+		System.out.println('3' - '0' + 1);
+		System.out.println(Integer.parseInt("3") + 1);
+		System.out.println("3" + 1);
+		System.out.println(3 + '0');
+	}
+
+}
---
+++ ch02/src/ScanfEx1.java
@@ -0,0 +1,14 @@
+import java.util.*; // 1. import문 추가 
+
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int n2 = sc.nextInt();
+		System.out.println(n);
+		System.out.println(n2);
+		sc.close();
+	}
+
+}
